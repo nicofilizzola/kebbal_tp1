191f30be67e84fc54647ebfa2a57f8e0a1aceb0d
Nicolás Filizzola <nicofilizola>
2021-01-30 16:50:13 +0100

    Initial commit

---
+++ Numerique.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace tp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Votre nombre : ");
+            string n = Console.ReadLine();
+            int IntN = Convert.ToInt32(n);
+            /*Numerique.sommePaire(IntN);*/
+            Numerique.coeffDecomp(IntN);
+
+        }
+    }
+
+     class Numerique
+    {
+        // q1
+        
+        public static void sommePaire(int n){
+            int sum = 0;
+            for (int counter = n; counter > 0; counter--){
+                if(counter % 2 == 0 && counter != n){
+                    sum += counter;
+                }
+            }
+            //return sum;
+            Console.WriteLine(sum);
+        }
+        
+
+        // q2
+        public static void coeffDecomp(int n){
+            string stringN = Convert.ToString(n);
+            int length = stringN.Length();
+        }
+    } 
+}
